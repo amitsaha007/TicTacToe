fef1cc832f8befb96b3fb663edf988f159e25f23
amitsaha007 <amitsaha1995@gmail.com>
2020-11-11 11:37:19 +0600

    Updated names, added a folder and a text file

--- version_2/Move.java
+++ version_2/Move.java
@@ -3,7 +3,7 @@
  *
  * STUDENTS: Nothing to change here.
  *
- * @author Andrew Vardy
+ * @author Amit Saha
  */
 
 public class Move {
--- version_2/SmartAI.java
+++ version_2/SmartAI.java
@@ -3,7 +3,7 @@
  *
  * STUDENTS: Nothing to change here.
  *
- * @author Andrew Vardy
+ * @author Amit Saha
  */
 
 import java.lang.UnsupportedOperationException;
